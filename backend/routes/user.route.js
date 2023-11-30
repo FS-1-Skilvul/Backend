fd7714ab6873ac21480fe72ea7aceafa700bb4cd
rivazahra <riva.zahra88@gmail.com>
2023-12-01 01:07:10 +0700

    add get all user

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getUserByToken, updateUserByToken } = require("../controllers/user.controlller");
+const { getUserByToken, updateUserByToken, getAllUser } = require("../controllers/user.controlller");
+const { verifyRoleAdmin } = require("../middleware/token");
 
 const route = express.Router();
 
 route.get("/", getUserByToken);
+route.get("/all",verifyRoleAdmin, getAllUser);
 route.put("/", updateUserByToken);
 
 module.exports = route;
